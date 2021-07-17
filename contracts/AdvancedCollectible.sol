3573be7998df5859fdae0604cd09080d24e9a824
Muhammad Tifak <moht20009@gmail.com>
2021-07-16 20:57:00 -0400

    Created contract constructor

--- contracts/AdvancedCollectible.sol
+++ contracts/AdvancedCollectible.sol
@@ -2,3 +2,15 @@ pragma solidity 0.6.6;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@chainlink/contracts/src/v0.6/VRFConsumerBase.sol";
+
+contract AdvancedCollectible is ERC721, VRFConsumerBase {
+    constructor(
+        address _VRFCoordinator,
+        address _LinkToken,
+        bytes32 _keyhash
+    )
+        public
+        VRFConsumer(_VRFCoordinator, _LinkToken)
+        ERC721("Heroes", "HERO")
+    {}
+}
