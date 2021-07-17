b7e6f1ef2f792557eac43d34590abc33e34dbb4b
Muhammad Tifak <moht20009@gmail.com>
2021-07-16 21:19:15 -0400

    Created Hero collectible

--- contracts/AdvancedCollectible.sol
+++ contracts/AdvancedCollectible.sol
@@ -4,13 +4,28 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@chainlink/contracts/src/v0.6/VRFConsumerBase.sol";
 
 contract AdvancedCollectible is ERC721, VRFConsumerBase {
+    bytes32 internal keyHash;
+    uint256 public fee;
+    mapping(bytes32 => address) public requestIdToSender;
+    mapping(bytes32 => string) public requestIdToTokenURI;
+    event requestedCollectible(bytes32 indexed requestId);
+
     constructor(
         address _VRFCoordinator,
         address _LinkToken,
         bytes32 _keyhash
-    )
+    ) public VRFConsumer(_VRFCoordinator, _LinkToken) ERC721("Heroes", "HERO") {
+        keyHash = _keyhash;
+        fee = 0.1 * 10**18; // 0.1 LINK
+    }
+
+    function createCollectible(uint256 userProvidedSeed, string memory tokenURI)
         public
-        VRFConsumer(_VRFCoordinator, _LinkToken)
-        ERC721("Heroes", "HERO")
-    {}
+        returns (bytes32)
+    {
+        bytes32 requestId = requestRandomness(keyHash, fee, userProvidedSeed);
+        requestIdToSender[requestId] = msg.sender;
+        requestIdToTokenURI[requestId] = tokenURI;
+        emit requestedCollectible(requestedId);
+    }
 }
