6e0a4a57cf206e11b74817f83b1662ebd1f828e9
Muhammad Tifak <moht20009@gmail.com>
2021-07-18 21:06:16 -0400

    Created tokenURI collectible and NFT on Opensea

---
+++ scripts/advanced_collectible/set_tokenuri.py
@@ -0,0 +1,37 @@
+from brownie import AdvancedCollectible, network, accounts, config
+from scripts.helpful_scripts import get_meta
+
+hero_metadata_dic = {
+    "ALIEN": "https://ipfs.io/ipfs/QmamJ18UXv17eDUk1YFJUfWQazbFsoR8p3ZjpZXAMEFQ5d?filename=alien.png",
+}
+
+OPENSEA_FORMAT = "https://testnets.opensea.io/assets/{}/{}"
+
+
+def main():
+    print("Working on " + network.show_active())
+    advanced_collectible = AdvancedCollectible[len(AdvancedCollectible) - 1]
+    number_of_advanced_collectibles = advanced_collectible.tokenCounter()
+    print(
+        "The number of tokens you've deployed is: "
+        + str(number_of_advanced_collectibles)
+    )
+    for token_id in range(number_of_advanced_collectibles):
+        meta = get_meta(advanced_collectible.tokenIdToMeta(token_id))
+        if not advanced_collectible.tokenURI(token_id).startswith("https://"):
+            print("Setting tokenURI of {}".format(token_id))
+            set_tokenURI(token_id, advanced_collectible,
+                         hero_metadata_dic[meta])
+        else:
+            print("Skipping {}, we already set that tokenURI!".format(token_id))
+
+
+def set_tokenURI(token_id, nft_contract, tokenURI):
+    dev = accounts.add(config["wallets"]["from_key"])
+    nft_contract.setTokenURI(token_id, tokenURI, {"from": dev})
+    print(
+        "Awesome! You can view your NFT at {}".format(
+            OPENSEA_FORMAT.format(nft_contract.address, token_id)
+        )
+    )
+    print('Please give up to 20 minutes, and hit the "refresh metadata" button.')
