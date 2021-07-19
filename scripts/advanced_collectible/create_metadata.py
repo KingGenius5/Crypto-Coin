9512a6172ec192647e12cd36642f65d090decfdf
Muhammad Tifak <moht20009@gmail.com>
2021-07-18 20:24:37 -0400

    Created metadata for IPFS

--- scripts/advanced_collectible/create_metadata.py
+++ scripts/advanced_collectible/create_metadata.py
@@ -2,6 +2,14 @@ from brownie import AdvancedCollectible, network
 from metadata import sample_metadata
 from scripts.helpful_scripts import get_meta
 from pathlib import Path
+import os
+import requests
+import json
+
+
+meta_to_image_uri = {
+    "ALIEN": "https://ipfs.io/ipfs/QmamJ18UXv17eDUk1YFJUfWQazbFsoR8p3ZjpZXAMEFQ5d?filename=alien.png",
+}
 
 
 def main():
@@ -30,3 +38,33 @@ def write_metadata(number_of_tokens, nft_contract):
             collectible_metadata["description"] = "Having the powers of {} doesn't stop this awesome person from killing it in the software engineering game!".format(
                 collectible_metadata["name"])
             print(collectible_metadata)
+            image_to_upload = None
+            if os.getenv("UPLOAD_IPFS") == "true":
+                image_path = "./img/{}.png".format(
+                    meta.lower().replace("_", "-"))
+                image_to_upload = upload_to_ipfs(
+                    image_path)
+            image_to_upload = meta_to_image_uri[meta] if not image_to_upload else image_to_upload
+            collectible_metadata["image"] = image_to_upload
+            with open(metadata_file_name, "w") as file:
+                json.dump(collectible_metadata, file)
+            if os.getenv("UPLOAD_IPFS") == "true":
+                upload_to_ipfs(metadata_file_name)
+
+# curl -X POST file=@img/stealth.png http://localhost:5001/api/v0/add
+# We want to do ^ but in a function instead of through the terminal
+
+
+def upload_to_ipfs(filepath):
+    with Path(filepath).open("rb") as fp:
+        image_binary = fp.read()
+        ipfs_url = "http://localhost:5001"
+        response = requests.post(ipfs_url + "/api/v0/add",
+                                 files={"file": image_binary})
+        ipfs_hash = response.json()["Hash"]
+        filename = filepath.split("/")[-1:][0]
+        uri = "https://ipfs.io/ipfs/{}?filename={}".format(
+            ipfs_hash, filename)
+        print(uri)
+        return uri
+    return None
